af9135685fc0a2319dfc1b874aea91d0fd7d0831
Tanishq Dubey <tdubey@clearstreet.io>
2019-12-20 12:07:30 -0600

    mypy 100, flake8, 100, test 100

--- gestalt.py
+++ gestalt.py
@@ -1,33 +1,24 @@
 import os
 import glob
 import json
-import collections.abc
-from typing import Dict, List, Type, Any, Union
+import collections.abc as collections
+from typing import Dict, List, Type, Union, Optional, MutableMapping
 
-"""
-    explicit call to Set
-    flag -- V2
-    env
-    config
-    key/value store -- not doing (VX)
-    call default
-    config default
-"""
 
 class Gestalt:
     def __init__(self):
         self.__conf_data: Dict = dict()
-        self.__conf_file_format = 'json'
-        self.__conf_file_name = '*'
-        self.__conf_file_paths = []
-        self.__use_env = False
-        self.__env_prefix = ''
-        self.__delim_char = '.'
+        self.__conf_file_format: str = 'json'
+        self.__conf_file_name: str = '*'
+        self.__conf_file_paths: list = []
+        self.__use_env: bool = False
+        self.__env_prefix: str = ''
+        self.__delim_char: str = '.'
         self.__conf_sets: Dict = dict()
         self.__conf_defaults: Dict = dict()
 
-    def __flatten(self, d:Dict, parent_key='', sep='.'):
-        items = []
+    def __flatten(self, d: MutableMapping, parent_key: str = '', sep: str = '.'):
+        items: list = []
         for k, v in d.items():
             new_key = parent_key + sep + k if parent_key else k
             if isinstance(v, collections.MutableMapping):
@@ -36,7 +27,7 @@ class Gestalt:
                 items.append((new_key, v))
         return dict(items)
 
-    def add_config_path(self, path:str):
+    def add_config_path(self, path: str):
         tmp = os.path.abspath(path)
         print(tmp)
         if not os.path.lexists(tmp):
@@ -59,7 +50,7 @@ class Gestalt:
         self.__use_env = True
         self.__env_prefix = ''
 
-    def __set(self, key:str, value:Union[str, int, float, bool, list], t:Type):
+    def __set(self, key: str, value: Union[str, int, float, bool, list], t: Type):
         if not isinstance(key, str):
             raise TypeError(f'Given key is not of string type')
         if not isinstance(value, t):
@@ -68,52 +59,58 @@ class Gestalt:
             raise TypeError(f'Overriding key {key} with type {type(self.__conf_sets[key])} with a {t} is not permitted')
         self.__conf_sets[key] = value
 
-    def set_string(self, key:str, value:str):
+    def set_string(self, key: str, value: str):
         self.__set(key, value, str)
 
-    def set_int(self, key:str, value:int):
+    def set_int(self, key: str, value: int):
         self.__set(key, value, int)
 
-    def set_float(self, key:str, value:float):
+    def set_float(self, key: str, value: float):
         self.__set(key, value, float)
 
-    def set_bool(self, key:str, value:bool):
+    def set_bool(self, key: str, value: bool):
         self.__set(key, value, bool)
 
-    def set_list(self, key:str, value:list):
+    def set_list(self, key: str, value: list):
         self.__set(key, value, list)
 
-    def __set_default(self, key:str, value:Union[str, int, float, bool, list], t:Type):
+    def __set_default(self, key: str, value: Union[str, int, float, bool, list], t: Type):
         if not isinstance(key, str):
             raise TypeError(f'Given key is not of string type')
         if not isinstance(value, t):
             raise TypeError(f'Input value when setting default {t} of type {type(value)} is not permitted')
         if key in self.__conf_defaults and not isinstance(self.__conf_defaults[key], t):
-            raise TypeError(f'Overriding default key {key} with type {type(self.__conf_defaults[key])} with a {t} is not permitted')
+            raise TypeError(f'Overriding default key {key} \
+                    with type {type(self.__conf_defaults[key])} with a {t} is not permitted')
         self.__conf_defaults[key] = value
 
-    def set_default_string(self, key:str, value:str):
+    def set_default_string(self, key: str, value: str):
         self.__set_default(key, value, str)
 
-    def set_default_int(self, key:str, value:str):
+    def set_default_int(self, key: str, value: int):
         self.__set_default(key, value, int)
 
-    def set_default_float(self, key:str, value:str):
+    def set_default_float(self, key: str, value: float):
         self.__set_default(key, value, float)
 
-    def set_default_bool(self, key:str, value:str):
+    def set_default_bool(self, key: str, value: bool):
         self.__set_default(key, value, bool)
 
-    def set_default_list(self, key:str, value:str):
+    def set_default_list(self, key: str, value: list):
         self.__set_default(key, value, list)
 
-    def __get(self, key:str, default:Union[str, int, float, bool, list], t:Type) -> Union[str, int, float, bool, list]:
+    def __get(self, key: str,
+              default: Optional[Union[str, int, float, bool, list]],
+              t: Type) -> Union[str, int, float, bool, list]:
         if not isinstance(key, str):
             raise TypeError(f'Given key is not of string type')
         if default and not isinstance(default, t):
             raise TypeError(f'Provided default is of incorrect type {type(default)}, it should be of type {t}')
         if key in self.__conf_sets:
-            return self.__conf_sets[key]
+            val = self.__conf_sets[key]
+            if not isinstance(val, t):
+                raise TypeError(f'Given set key is not of type {t}, but of type {type(val)}')
+            return val
         if self.__use_env:
             e_key = key.upper().replace(self.__delim_char, '_')
             if e_key in os.environ:
@@ -128,24 +125,41 @@ class Gestalt:
         if default:
             return default
         if key in self.__conf_defaults:
-            return self.__conf_defaults[key]
+            val = self.__conf_defaults[key]
+            if not isinstance(val, t):
+                raise TypeError(f'Given default set key is not of type {t}, but of type {type(val)}')
+            return val
         raise ValueError(f'Given key {key} is not in any configuration and no default is provided')
 
-    def get_string(self, key:str, default:str = None) -> str:
-        return self.__get(key, default, str)
+    def get_string(self, key: str, default: str = None) -> str:
+        val: Union[str, int, float, bool, list] = self.__get(key, default, str)
+        if not isinstance(val, str):
+            raise RuntimeError(f'Gestalt error: expected to return string, but got {type(val)}')
+        return val
 
-    def get_int(self, key:str, default:int = None) -> int:
-        return self.__get(key, default, int)
+    def get_int(self, key: str, default: int = None) -> int:
+        val: Union[str, int, float, bool, list] = self.__get(key, default, int)
+        if not isinstance(val, int):
+            raise RuntimeError(f'Gestalt error: expected to return string, but got {type(val)}')
+        return val
 
-    def get_float(self, key:str, default:int = None) -> float:
-        return self.__get(key, default, float)
+    def get_float(self, key: str, default: float = None) -> float:
+        val: Union[str, int, float, bool, list] = self.__get(key, default, float)
+        if not isinstance(val, float):
+            raise RuntimeError(f'Gestalt error: expected to return float, but got {type(val)}')
+        return val
 
-    def get_bool(self, key:str, default:bool = None) -> bool:
-        return self.__get(key, default, bool)
+    def get_bool(self, key: str, default: bool = None) -> bool:
+        val: Union[str, int, float, bool, list] = self.__get(key, default, bool)
+        if not isinstance(val, bool):
+            raise RuntimeError(f'Gestalt error: expected to return bool, but got {type(val)}')
+        return val
 
-    def get_list(self, key:str, default:bool = None) -> List:
-        return self.__get(key, default, list)
+    def get_list(self, key: str, default: list = None) -> List:
+        val: Union[str, int, float, bool, list] = self.__get(key, default, list)
+        if not isinstance(val, list):
+            raise RuntimeError(f'Gestalt error: expected to return list, but got {type(val)}')
+        return val
 
     def dump(self) -> str:
         return str(json.dumps(self.__conf_data, indent=4))
-
--- test_gestalt.py
+++ test_gestalt.py
@@ -2,6 +2,7 @@ import pytest
 import os
 from gestalt import gestalt
 
+
 # Testing JSON Loading
 def test_loading_json():
     g = gestalt.Gestalt()
@@ -10,17 +11,20 @@ def test_loading_json():
     x = g.dump()
     assert len(x)
 
+
 def test_loading_json_nonexist_dir():
     g = gestalt.Gestalt()
     with pytest.raises(ValueError) as terr:
         g.add_config_path('./nonexistpath')
         assert 'does not exist' in terr
 
+
 def test_loading_json_file_not_dir():
     g = gestalt.Gestalt()
     with pytest.raises(ValueError) as terr:
         g.add_config_path('./gestalt.py')
         assert 'is not a directory' in terr
+
 
 def test_get_wrong_type():
     g = gestalt.Gestalt()
@@ -30,6 +34,7 @@ def test_get_wrong_type():
         g.get_string('numbers')
         assert 'is not of type' in terr
 
+
 def test_get_non_exist_key():
     g = gestalt.Gestalt()
     g.add_config_path('./testdata')
@@ -37,6 +42,7 @@ def test_get_non_exist_key():
     with pytest.raises(ValueError) as terr:
         g.get_string('non-exist')
         assert 'is not in any configuration and no default is provided' in terr
+
 
 def test_get_key_wrong_type():
     g = gestalt.Gestalt()
@@ -46,6 +52,7 @@ def test_get_key_wrong_type():
         g.get_string(1234)
         assert 'is not of string type' in terr
 
+
 def test_get_key_wrong_default_type():
     g = gestalt.Gestalt()
     g.add_config_path('./testdata')
@@ -54,6 +61,7 @@ def test_get_key_wrong_default_type():
         g.get_string('nonexist', 1234)
         assert 'Provided default is of incorrect type' in terr
 
+
 def test_get_json_string():
     g = gestalt.Gestalt()
     g.add_config_path('./testdata')
@@ -61,12 +69,14 @@ def test_get_json_string():
     testval = g.get_string('yarn')
     assert testval == 'blue skies'
 
+
 def test_get_json_default_string():
     g = gestalt.Gestalt()
     g.add_config_path('./testdata')
     g.build_config()
     testval = g.get_string('nonexist', 'mydefval')
     assert testval == 'mydefval'
+
 
 def test_get_json_set_default_string():
     g = gestalt.Gestalt()
@@ -76,12 +86,14 @@ def test_get_json_set_default_string():
     testval = g.get_string('nonexisttest')
     assert testval == 'otherdefval'
 
+
 def test_get_json_int():
     g = gestalt.Gestalt()
     g.add_config_path('./testdata')
     g.build_config()
     testval = g.get_int('numbers')
     assert testval == 12345678
+
 
 def test_get_json_float():
     g = gestalt.Gestalt()
@@ -90,12 +102,14 @@ def test_get_json_float():
     testval = g.get_float('strangenumbers')
     assert testval == 123.456
 
+
 def test_get_json_bool():
     g = gestalt.Gestalt()
     g.add_config_path('./testdata')
     g.build_config()
     testval = g.get_bool('truthy')
-    assert testval == True
+    assert testval is True
+
 
 def test_get_json_list():
     g = gestalt.Gestalt()
@@ -106,12 +120,14 @@ def test_get_json_list():
     assert 'dog' in testval
     assert 'cat' in testval
 
+
 def test_get_json_nested():
     g = gestalt.Gestalt()
     g.add_config_path('./testdata')
     g.build_config()
     testval = g.get_string('deep.nested1')
     assert testval == 'hello'
+
 
 # Test Set Overriding
 def test_set_string():
@@ -120,11 +136,13 @@ def test_set_string():
     testval = g.get_string('mykey')
     assert testval == 'myval'
 
+
 def test_set_int():
     g = gestalt.Gestalt()
     g.set_int('mykey', 1234)
     testval = g.get_int('mykey')
     assert testval == 1234
+
 
 def test_set_float():
     g = gestalt.Gestalt()
@@ -132,19 +150,30 @@ def test_set_float():
     testval = g.get_float('mykey')
     assert testval == 45.23
 
+
 def test_set_bool():
     g = gestalt.Gestalt()
     g.set_bool('mykey', False)
     testval = g.get_bool('mykey')
-    assert testval == False
+    assert testval is False
+
 
 def test_set_list():
     g = gestalt.Gestalt()
     g.set_list('mykey', ['hi', 'bye'])
-    testval = g.get_string('mykey')
+    testval = g.get_list('mykey')
     assert testval
     assert 'hi' in testval
     assert 'bye' in testval
+
+
+def test_set_int_get_bad():
+    g = gestalt.Gestalt()
+    g.set_int('mykey', 1234)
+    with pytest.raises(TypeError) as terr:
+        g.get_string('mykey')
+        assert 'Given set key is not of type' in terr
+
 
 def test_set_bad_key_type():
     g = gestalt.Gestalt()
@@ -152,11 +181,13 @@ def test_set_bad_key_type():
         g.set_string(1234, 'myval')
         assert 'is not of string type' in terr
 
+
 def test_set_bad_type():
     g = gestalt.Gestalt()
     with pytest.raises(TypeError) as terr:
         g.set_string('mykey', 123)
         assert 'is not of type' in terr
+
 
 def test_re_set_bad_type():
     g = gestalt.Gestalt()
@@ -164,6 +195,7 @@ def test_re_set_bad_type():
     with pytest.raises(TypeError) as terr:
         g.set_int('mykey', 123)
         assert 'Overriding key' in terr
+
 
 def test_set_override():
     g = gestalt.Gestalt()
@@ -175,6 +207,7 @@ def test_set_override():
     testval = g.get_int('numbers')
     assert testval == 6543
 
+
 # Test Env Variables
 def test_get_env_string():
     g = gestalt.Gestalt()
@@ -183,6 +216,7 @@ def test_get_env_string():
     testval = g.get_string('mykey')
     assert testval == 'myval'
 
+
 def test_get_env_int():
     g = gestalt.Gestalt()
     g.auto_env()
@@ -190,12 +224,14 @@ def test_get_env_int():
     testval = g.get_int('mykey')
     assert testval == 999
 
+
 def test_get_nested_env_string():
     g = gestalt.Gestalt()
     g.auto_env()
     os.environ['MY_KEY'] = 'myval'
     testval = g.get_string('my.key')
     assert testval == 'myval'
+
 
 def test_get_env_bad_type():
     g = gestalt.Gestalt()
@@ -205,6 +241,7 @@ def test_get_env_bad_type():
         g.get_int('my.key')
         assert "could not be converted to type" in terr
 
+
 # Test Default Values
 def test_set_default_string():
     g = gestalt.Gestalt()
@@ -212,31 +249,44 @@ def test_set_default_string():
     testval = g.get_string('mykey')
     assert testval == 'myval'
 
+
 def test_set_default_int():
     g = gestalt.Gestalt()
     g.set_default_int('mykey', 1234)
-    testval = g.get_string('mykey')
+    testval = g.get_int('mykey')
     assert testval == 1234
+
 
 def test_set_default_float():
     g = gestalt.Gestalt()
     g.set_default_float('mykey', 1234.05)
-    testval = g.get_string('mykey')
+    testval = g.get_float('mykey')
     assert testval == 1234.05
+
 
 def test_set_default_bool():
     g = gestalt.Gestalt()
     g.set_default_bool('mykey', False)
-    testval = g.get_string('mykey')
-    assert testval == False
+    testval = g.get_bool('mykey')
+    assert testval is False
+
 
 def test_set_default_list():
     g = gestalt.Gestalt()
     g.set_default_list('mykey', ['bear', 'bull'])
-    testval = g.get_string('mykey')
+    testval = g.get_list('mykey')
     assert testval
     assert 'bear' in testval
     assert 'bull' in testval
+
+
+def test_set_default_int_get_bad():
+    g = gestalt.Gestalt()
+    g.set_default_int('mykey', 1234)
+    with pytest.raises(TypeError) as terr:
+        g.get_string('mykey')
+        assert 'Given default set key is not of type' in terr
+
 
 def test_set_default_string_bad_key():
     g = gestalt.Gestalt()
@@ -244,11 +294,13 @@ def test_set_default_string_bad_key():
         g.set_default_string(1234, 'myval')
         assert 'Given key is not of string type' in terr
 
+
 def test_set_default_string_bad_val():
     g = gestalt.Gestalt()
     with pytest.raises(TypeError) as terr:
         g.set_default_string('mykey', 123)
         assert 'Input value when setting default' in terr
+
 
 def test_set_default_string_bad_val_override():
     g = gestalt.Gestalt()
@@ -256,4 +308,3 @@ def test_set_default_string_bad_val_override():
         g.set_default_string('mykey', 'myval')
         g.set_default_int('mykey', 1234)
         assert 'Overriding default key' in terr
-
