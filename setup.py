e303fb7d09c504facdfd05bb0416a9c5675d2d78
Adisun Wheelock <32372583+adisunw@users.noreply.github.com>
2020-01-15 19:06:42 -0500

    Fix build issues

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='gestalt-cfg',
-      version='1.0.1',
+      version='1.0.2',
       description='A sensible configuration library for Python',
       long_description=readme(),
       long_description_content_type="text/markdown",
