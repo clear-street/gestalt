efb653dd07b2b78088768e0aabead423218e567f
Vishesh Gupta <vishesh.gupta12@outlook.com>
2021-04-01 16:22:47 +0000

    Removed utility.py

--- gestalt/utility.py
+++
@@ -1,8 +0,0 @@
-from ConfigProvider import ConfigProvider
-from typing import Dict
-
-config_provider: Dict[str, ConfigProvider] = {}
-
-def new_config_provider_registry() -> Dict[str, ConfigProvider]:
-        config_provider: Dict[str, ConfigProvider] = {} 
-        return config_provider
