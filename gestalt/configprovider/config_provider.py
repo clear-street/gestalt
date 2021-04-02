4815582eaadc43b3f626fe44f6e73ddde3625bf9
Vishesh Gupta <vishesh.gupta12@outlook.com>
2021-04-02 00:35:29 +0000

    Added ConfigProvider and restructured due to circular depenedancy

--- gestalt/configprovider/config_provider.py
+++ gestalt/configprovider/config_provider.py
@@ -1,42 +1,62 @@
 from abc import abstractclassmethod
-from gestalt.configprovider.config_provider_registry import ConfigProviderRegistry
-from typing import Dict, Type
-from . import config_provider
+from typing import Dict, Union
 from gestalt.remote_provider import RemoteProvider
 
+config_provider: Dict[str, any] = {}
+
+class ConfigProviderRegistry:
+    """The registry for Config Providers
+    """
+    config_providers: Dict[str, any] = config_provider
+
+    def register_provider(self, name: str, provider: any) -> None:
+        """Receives a provider and registers it in the config_providers
+
+        Args:
+            name (str): The name of the provider
+            provider (ConfigProvider): The provider for the name
+        """
+        self.config_providers[name] = provider
+
+    def get_config_provider(self, rp: RemoteProvider) -> Union[any, bool]:
+        """Gets a config providers from the list of remote providers
+
+        Args:
+            rp (RemoteProvider): Remote Provider that needs to be found
+
+        Returns:
+            config_provider: If config_provider for remote_provider is found, else boolean
+        """
+        provider = rp.provider
+        if self.config_providers[provider]:
+            return self.config_providers[provider]
+        return False
+
 class ConfigProvider:
+    """ConfigProvider is the interface defined by Gestalt for remote config providers
     """
-    ConfigProvider is the interface defined by Gestalt for remote config providers
+
+    config_provider_registry: Dict = ConfigProviderRegistry()
+ 
+    @abstractclassmethod
+    def Get(rp: RemoteProvider) -> any:
+        """Get Abstract Method which gets implemented by the sub class that inherits
+        ConfigProvider
+
+        Args:
+            rp: RemoteProvider
+
+        Returns:
+            any: The base class adds the restriction
+        """
+        pass
+
+
+def new_config_provider_registry() -> Dict[str, ConfigProvider]:
+    """Generates a new ConfigProviderRegistry
+
+    Returns:
+        config_provider_registry: a new dictionary with fomat Dict[str, ConfigProvider]
     """
-    config_provider_registry = ConfigProviderRegistry()
-
-    def register(self, name, provider):
-        self.config_provider_registry
-
-    # The type of client is key: string, value: hvac.client 
-    @abstractclassmethod
-    def Get(rp: RemoteProvider):
-        """
-        Args:
-            rp: RemoteProvider
-        """
-        pass
-
-    @abstractclassmethod
-    def Watch():
-        """
-        Args:
-            rp: RemoteProvider
-        """
-        pass
-
-    @abstractclassmethod
-    def WatchChannel():
-        """
-        Args:
-            rp: RemoteProvider
-        """
-        pass
-
-
-
+    config_provider_registry: Dict[str, ConfigProvider] = {} 
+    return config_provider_registry
--- gestalt/configprovider/global.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict
-from gestalt.configprovider.config_provider import ConfigProvider
-
-config_provider: Dict[str, ConfigProvider] = {}
-
-def new_config_provider_registry() -> Dict[str, ConfigProvider]:
-        config_provider: Dict[str, ConfigProvider] = {} 
-        return config_provider
