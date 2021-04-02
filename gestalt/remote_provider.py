9f3b40ff30d884cef7eba24ff905f8e269e3b6c3
Vishesh Gupta <vishesh.gupta12@outlook.com>
2021-04-02 00:20:00 +0000

    Added the remote_provider

--- gestalt/remote_provider.py
+++ gestalt/remote_provider.py
@@ -1,40 +1,21 @@
-from typing import List
+from typing import List, Union
 
 SupportedRemoteProviders: List[str] = ["etcd", "consul", "firestore"]
 
 class RemoteProvider:
-    def __init__(self):
-        self.provider = str()
-        self.endpoint = str()
-        self.path = str()
-        self.secret_keyring = str()
+    """RemoteProvider class that generates a Remote Provider
+    """
 
-    def provider(self) -> str:
-        return self.provider
+    def __init__(self, provider: str, endpoint: str, path: str, secret_keystring: Union[str, None]=None):
+        """ Initializes the remote provider class
 
-    def endpoint(self) -> str:
-        return self.endpoint
-
-    def path(self) -> str:
-        return self.path
-
-    def secret_keyring(self) -> str:
-        return self.secretKeyring
-
-    def add_remote_provider(self, provider: str, endpoint: str, path: str) -> None:
-        if provider:
-            self.provider = provider 
-        if endpoint:
-            self.endpoint = endpoint
-        if path:
-            self.path = self.path
-
-    def addRemoteProvider(self, provider: str, endpoint: str, path: str, secret_keyring: str) -> None:
-        if provider: 
-            self.provider = provider
-        if endpoint:
-            self.endpoint = endpoint
-        if path:
-            self.path = path
-        if secret_keyring:
-            self.secret_keyring = secret_keyring
+        Args:
+            provider (str): The name of the provider
+            endpoint (str): The endpoint of the provider
+            path (str): The path to the provider 
+            secret_keystring (str | None): The secret_keystring
+        """
+        self.provider = provider
+        self.endpoint = endpoint
+        self.path = path
+        self.secret_keystring = secret_keystring
