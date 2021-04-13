fce4c6dfe97eb0b95f1eb54ebcc21069e97541a1
Vishesh Gupta <vishesh.gupta12@outlook.com>
2021-04-13 13:27:52 +0000

    Cleaned the up the call to certs

--- gestalt/stubs/__init__.pyi
+++ gestalt/stubs/__init__.pyi
@@ -1,9 +1,14 @@
-from typing import Tuple, TypedDict, Union
+from typing import NamedTuple, TypedDict, Union
+import typing
+
+class CACertClient(NamedTuple):
+    client_cert_path: str
+    client_key_path: str
 
 class HVAC_ClientConfig(TypedDict):
     url: str
     token: str
-    cert: Union[None, Tuple]
+    cert: Union[None, CACertClient]
     verify: Union[None, bool]
 
 class HVAC_ClientAuthentication(TypedDict):
