4ea6986fe58f59e03a1b7236d4a8d35d75199f38
Jose Boucourt <jboucourt@clearstreet.io>
2024-08-13 21:05:26 +0000

    Updating

--- gestalt/vault.py
+++ gestalt/vault.py
@@ -58,7 +58,7 @@ class Vault(Provider):
 
         self.delay = delay
         self.tries = tries
-        self.kubernetes_ttl_renew = None
+        self.kubernetes_ttl_renew: Optional[Thread] = None
 
     @property
     def vault_client(self) -> hvac.Client:
