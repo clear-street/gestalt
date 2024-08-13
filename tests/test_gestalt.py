2023ad1b1cd0efeb2193addb25bc8f7f1bcb79a4
Jose Boucourt <jboucourt@clearstreet.io>
2024-08-13 20:57:36 +0000

    Updating tests

--- tests/test_gestalt.py
+++ tests/test_gestalt.py
@@ -611,7 +611,7 @@ def test_vault_worker_k8s(mock_vault_workers):
         with patch("gestalt.vault.hvac.Client") as mock_client:
             v = Vault(role="test-role", jwt="test-jwt")
             v.connect()
-            v.kube_token = ("dynamic", 1, 100)
+            v.kube_token = ("kubernetes", 1, 100)
 
             mock_k8s_renew.start.assert_called()
 
